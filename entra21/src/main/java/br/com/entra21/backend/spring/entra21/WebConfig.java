0d5c4a1111c09122ef6cc53a43a2f20039ad6f9f
Ste-Viera <stephanie_holzschuk@hotmail.com>
2022-09-16 18:57:55 -0300

    Aula Spring

---
+++ entra21/src/main/java/br/com/entra21/backend/spring/entra21/WebConfig.java
@@ -0,0 +1,17 @@
+package br.com.entra21.backend.spring.entra21;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("*") 
+		.allowedMethods("GET","POST","PUT", "DELETE"); 
+
+	}
+
+	
+}
